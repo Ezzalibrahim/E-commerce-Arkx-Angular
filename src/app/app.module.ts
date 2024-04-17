3392620d6d0971866417ed6b6493c7e9fc522749
Ezzalibrahim <brahim.ezzali@hotmail.com>
2024-04-17 11:02:06 +0100

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ZoomDirective } from './directives/zoom.directive';
+import { CouponPipe } from './pipes/coupon.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCardComponent,
+    ZoomDirective,
+    CouponPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
